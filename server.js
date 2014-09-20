62fadda267ff5f023174121f422b7494cd93797c
kwilson <kwilson@ibigroup.com>
2014-09-20 12:57:32 +0100

    Reduced the response size and changed query URL.

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ var http = require('http');
 var restify = require('restify');
 var sentiment = require('sentiment');
 
+function formatResponse(response) {
+    return {
+        score: response.score,
+        positive: response.positive,
+        negative: response.negative
+    };
+}
+
 function respond(req, res, next) {
     var result = sentiment(req.params.content);
-    res.send(result);
+    res.send(formatResponse(result));
     next();
 }
 
 var server = restify.createServer();
-server.get('/analyse/:content', respond);
+server.get('/q/:content', respond);
 
 server.listen(process.env.PORT || 8080, function() {
     console.log('%s listening at %s', server.name, server.url);
